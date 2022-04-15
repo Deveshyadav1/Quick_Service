5011f7095747b94571eab650cfbb97c1be64a13f
Deveshyadav1 <Devesh11717653@gmail.com>
2022-04-15 21:55:16 +0530

    Add files via upload

---
+++ welcome.php
@@ -0,0 +1,25 @@
+<?php 
+
+session_start();
+
+if (!isset($_SESSION['username'])) {
+    header("Location: index.php");
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome</title>
+</head>
+<body>
+    <center>
+    <?php echo "<h1>Welcome " . $_SESSION['username'] . "</h1>"; ?>
+</center>
+    <a href="logout.php">Logout</a>
+
+</body>
+</html>
